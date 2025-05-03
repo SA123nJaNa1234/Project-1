14e37183a12793a0b41f8a712d99abd65dd5f3a2
SA123nJaNa1234 <sanjanakamat30@gmail.com>
2025-05-03 23:28:36 +0530

    restaurant-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,46 @@ module.exports = {
       xl: "1180px"
     },
 
-    extend: {},
-
-    fontFamily: {
-      Jost: ["Jost", "sans-serif"],
-      Lobster: ["Lobster", "sans-serif"]
+    extend: {
+      
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: '0' },
+            '100%': { opacity: '1' },
+          },
+          slideUp: {
+            '0%': { transform: 'translateY(50px)', opacity: '0' },
+            '100%': { transform: 'translateY(0)', opacity: '1' },
+          },
+          zoomIn: {
+            '0%': { transform: 'scale(0.8)', opacity: '0' },
+            '100%': { transform: 'scale(1)', opacity: '1' },
+          },
+        },
+        animation: {
+          fadeIn: 'fadeIn 1.5s ease-out forwards',
+          slideUp: 'slideUp 1.5s ease-out forwards',
+          zoomIn: 'zoomIn 1s ease-out forwards',
+        },
+        fontFamily: {
+          Jost: ["Jost", "sans-serif"],
+          Lobster: ["Lobster", "sans-serif"]
+        },
+    
+        container: {
+          center: true,
+          padding: {
+            DEFAULT: "12PX",
+            md: "32px"
+          },
+        },
     },
 
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: "12PX",
-        md: "32px"
-      }
-    }
-  },
+    
+
+   
+    },
+  
   plugins: [],
-};
+}
+
